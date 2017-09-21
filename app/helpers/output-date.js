49bb3dcd292d043a0bf387f5aff49e1c1163ffe6
Josh Messer <josh.messer@connxus.com>
2017-09-21 13:24:33 -0400

    Fixed date output

--- app/helpers/output-date.js
+++ app/helpers/output-date.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export function outputDate([ date ]/*, hash*/) {
   const d = new Date(date);
-  const m = d.getMonth();
+  const m = d.getMonth() + 1;
   const day = d.getDate();
   const y = d.getFullYear();
 
