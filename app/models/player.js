b037742330a665dbfcbbec078865dd28216faebd
Josh Messer <josh.messer@connxus.com>
2017-09-21 13:45:16 -0400

    Various visual updates

--- app/models/player.js
+++ app/models/player.js
@@ -8,51 +8,51 @@ const {
 const characters = [
   {
     name: 'Mario',
-    image: 'https://vignette.wikia.nocookie.net/nintendo/images/6/62/Mario_SSB.gif/revision/latest?cb=20071003034704&path-prefix=en'
+    image: 'https://www.ssbwiki.com/images/b/b3/Mario_SSB.png'
   },
   {
     name: 'Donkey Kong',
-    image: 'https://vignette.wikia.nocookie.net/nintendo/images/a/a2/DK_SSB.PNG/revision/latest?cb=20090113211606&path-prefix=en'
+    image: 'https://www.ssbwiki.com/images/3/3d/Donkey_Kong_SSB.png'
   },
   {
     name: 'Link',
-    image: 'https://vignette.wikia.nocookie.net/nintendo/images/d/d8/Link_SSB.gif/revision/latest?cb=20071003040250&path-prefix=en'
+    image: 'https://www.ssbwiki.com/images/6/6e/Link_SSB.png'
   },
   {
     name: 'Samus Aran',
-    image: 'https://vignette.wikia.nocookie.net/nintendo/images/3/30/Samus_SSB.PNG/revision/latest?cb=20120527181837&path-prefix=en'
+    image: 'https://www.ssbwiki.com/images/1/18/Samus_SSB.png'
   },
   {
     name: 'Yoshi',
-    image: 'https://vignette.wikia.nocookie.net/nintendo/images/f/f2/Yoshi_SSB.gif/revision/latest?cb=20071003045456&path-prefix=en'
+    image: 'https://www.ssbwiki.com/images/6/64/Yoshi_SSB.png'
   },
   {
     name: 'Kirby',
-    image: 'https://vignette.wikia.nocookie.net/nintendo/images/b/b2/Kirby_SSB.gif/revision/latest?cb=20071003043702&path-prefix=en'
+    image: 'https://www.ssbwiki.com/images/f/f2/Kirby_SSB.png'
   },
   {
     name: 'Fox McCloud',
-    image: 'https://vignette.wikia.nocookie.net/nintendo/images/c/ce/Fox_SSB.gif/revision/latest?cb=20070916042608&path-prefix=en'
+    image: 'https://www.ssbwiki.com/images/6/61/Fox_SSB.png'
   },
   {
     name: 'Pikachu',
-    image: 'https://vignette.wikia.nocookie.net/nintendo/images/c/cf/Pikachu_SSB.gif/revision/latest?cb=20071003035841&path-prefix=en'
+    image: 'https://www.ssbwiki.com/images/9/92/Pikachu_SSB.png'
   },
   {
     name: 'Luigi',
-    image: 'https://vignette.wikia.nocookie.net/nintendo/images/4/44/Luigi_SSB.gif/revision/latest?cb=20090113225102&path-prefix=en'
+    image: 'https://www.ssbwiki.com/images/5/52/Luigi_SSB.png'
   },
   {
     name: 'Captain Falcon',
-    image: 'https://vignette.wikia.nocookie.net/nintendo/images/0/04/CaptainfalconSSB.png/revision/latest?cb=20160215194058&path-prefix=en'
+    image: 'https://www.ssbwiki.com/images/0/04/Captain_Falcon_SSB.png'
   },
   {
     name: 'Ness',
-    image: 'https://vignette.wikia.nocookie.net/nintendo/images/3/30/Ness_SSB.gif/revision/latest?cb=20070915074503&path-prefix=en'
+    image: 'https://www.ssbwiki.com/images/1/16/Ness_SSB.png'
   },
   {
     name: 'Jigglypuff',
-    image: 'https://vignette.wikia.nocookie.net/nintendo/images/3/39/Jigglypuff_SSB.gif/revision/latest?cb=20090113231659&path-prefix=en'
+    image: 'https://www.ssbwiki.com/images/d/de/Jigglypuff_SSB.png'
   },
 ];
 
