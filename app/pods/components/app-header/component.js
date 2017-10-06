2407636e8b8a2b1e67ce2f301c4fe4988d07f7c8
Josh Messer <josh.messer@connxus.com>
2017-10-06 10:17:29 -0400

    Updating interface - WIP

---
+++ app/pods/components/app-header/component.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+const {
+    inject: { service }
+} = Ember;
+
+export default Ember.Component.extend({
+    session: service(),
+
+    actions: {
+        transition() {
+            this.transitionToRoute(...arguments);
+        }
+    }
+});
